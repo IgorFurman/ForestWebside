f7937df1b8497bc8b2c4deb7368e6c39e47c6754
Igor <igorfurman555@gmail.com>
2023-04-14 15:26:34 +0200

    fixed media querries and added scrollspy

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,6 @@ const navbar = document.querySelector('.nav');
 const navDetoskopItemLinks = document.querySelectorAll('.nav__item');
 const navMobileItemsLinks = document.querySelectorAll('.nav__mobile-item');
 
-const sections = document.querySelectorAll('section');
-
 //nav funtctions
 
 const blockScroll = () => {
@@ -67,16 +65,6 @@ const scrollToSection = (targetId) => {
 	});
 };
 
-// const scrollToSectionOnLoad = () => {
-// 	const currentUrl = window.location.href;
-// 	const currentHash = currentUrl.substring(currentUrl.indexOf('#'));
-// 	if (currentHash !== '') {
-// 		scrollToSection(currentHash);
-// 	}
-// };
-
-// window.addEventListener('load', scrollToSectionOnLoad);
-
 navDetoskopItemLinks.forEach((link) => {
 	link.addEventListener('click', (event) => {
 		event.preventDefault();
@@ -85,40 +73,74 @@ navDetoskopItemLinks.forEach((link) => {
 	});
 });
 
-//   mobile 
+//   mobile
 
 navMobileItemsLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-        setTimeout(() => {
-            window.scrollTo({
-                top: targetElement.offsetTop - navHeight,
-                behavior: 'smooth',
-            });
-        }, 0);
-    });
+	link.addEventListener('click', (event) => {
+		event.preventDefault();
+		const targetId = link.getAttribute('href');
+		const targetElement = document.querySelector(targetId);
+		setTimeout(() => {
+			window.scrollTo({
+				top: targetElement.offsetTop - navHeight,
+				behavior: 'smooth',
+			});
+		}, 0);
+	});
 });
 
-// scrollspy changing nav color;
+// scrollspy changing nav colors;
 
-// const updateNavbarColor = () => {
-// 	sections.forEach((section) => {
-// 		const sectionTop = section.offsetTop;
-// 		const sectionHeight = section.clientHeight;
-// 		if (
-// 			window.pageYOffset >= sectionTop - navHeight &&
-// 			window.pageYOffset < sectionTop + sectionHeight
-// 		) {
-// 			navbar.classList.add('active');
-// 		} else {
-// 			navbar.classList.remove('active');
-// 		}
-// 	});
-// };
+const changeNavbarColor = () => {
+	const sections = document.querySelectorAll('section');
 
-window.addEventListener('scroll', updateNavbarColor);
+	window.addEventListener('scroll', () => {
+		const scrollY = window.scrollY + navHeight;
+		let currentSectionIndex = 0;
+
+		sections.forEach((section, index) => {
+			const sectionTop = section.offsetTop;
+			const sectionBottom = sectionTop + section.clientHeight;
+			if (scrollY >= sectionTop && scrollY < sectionBottom) {
+				currentSectionIndex = index;
+				return;
+			}
+		});
+
+		switch (currentSectionIndex) {
+			case 0:
+				navbar.classList.remove('active');
+				navDetoskopItemLinks.forEach((link) => link.classList.remove('hover'));
+				break;
+
+			case 1:
+				navbar.classList.add('active');
+				navDetoskopItemLinks.forEach((link, index) => {
+					if (index === 0) {
+						link.classList.add('hover');
+					}
+				});
+				break;
+			case 2:
+				navbar.classList.remove('active');
+				navDetoskopItemLinks.forEach((link) => link.classList.remove('hover'));
+				break;
+			case 3:
+				navbar.classList.add('active');
+				navDetoskopItemLinks.forEach((link, index) => {
+					if (index === 1) {
+						link.classList.add('hover');
+					}
+				});
+				break;
+			default:
+				navbar.classList.remove('active');
+				break;
+		}
+	});
+};
+
+changeNavbarColor();
 
 // hiding nav while scrollinh down; --- DISABLED FOR NOW
 
@@ -132,7 +154,8 @@ window.addEventListener('scroll', updateNavbarColor);
 //   }
 //   prevScrollpos = currentScrollPos;
 // };
-// footer year
+
+// footer year ================================
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
